1a293df20c0a90e2b59e36776d4e840c2c838462
Niall ORourke <niall783@gmail.com>
2024-09-01 12:17:55 +0100

    added video and audio transcription

--- app/transcription/components/FileList.tsx
+++ app/transcription/components/FileList.tsx
@@ -205,7 +205,7 @@ const FileList: React.FC = () => {
                   <div className="mt-2">
                    
                     <p className="text-sm text-gray-500">
-                    Are you sure you want to delete the file '{fileToDelete?.name}'? This action cannot be undone.
+                    Are you sure you want to delete the file {fileToDelete?.name}? This action cannot be undone.
                     </p>
                   </div>
 
